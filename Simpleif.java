c4c83517964cf1f4435fec2757b084aaf00e1565
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-06-27 17:27:26 +0530

    Create Simpleif.java

---
+++ Simpleif.java
@@ -0,0 +1,13 @@
+//SIMPLEIF
+
+
+public class Simpleif{
+    public static void main (String args[]){
+        int a = 9;
+        int b = 5;
+        if(a+b < 20){
+            System.out.println("a + b is less than 20");
+        }
+    }
+}
+
