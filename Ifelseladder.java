b9942bbcf3f8220ef808e5a3bcb1c62c9446f447
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-06-27 17:25:26 +0530

    Create Ifelseladder.java

---
+++ Ifelseladder.java
@@ -0,0 +1,23 @@
+
+
+// IFELSE LADDER
+
+
+public class Ifelseladder {
+    public static void main (String args[]){
+    String name = "Daku";
+    if(name == "Saikat" ){
+        System.out.println("NAME IS SAIKAT");
+    }
+    else if(name == "Atactoc"){
+        System.out.println("NAME IS ATACTOC");
+    }
+    else if (name == "Arin"){
+        System.out.println("NAME IS ARIN");
+    }
+    else{
+        System.out.println(name);
+    }
+}
+
+}
