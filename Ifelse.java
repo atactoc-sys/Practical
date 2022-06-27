e233bb0b2be9210bc1d39fd5bdc3cb319a72e6cf
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-06-27 17:26:23 +0530

    Create Ifelse.java

---
+++ Ifelse.java
@@ -0,0 +1,18 @@
+
+// if else
+
+
+
+
+public class Ifelse{
+    public static void main (String args[]){
+        int a = 9;
+        int b = 15;
+        if(a+b < 20){
+            System.out.println("a + b is less than 20");
+        }
+        else {
+            System.out.println("a + b is greater tham 20");
+        }
+    }    
+}
