51e50bba0623e987d1189b1594e7dc34c58982c2
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-06-27 17:22:47 +0530

    Create Wjileloop.java

---
+++ Wjileloop.java
@@ -0,0 +1,23 @@
+// Whileloop
+
+
+
+
+
+
+
+
+
+
+public class Whilelloop {
+    public static void main (String args[]){
+        int i = 0;
+        System.out.println("FIRST 10 EVEN NUMBERS \n");
+        
+        while(i<=10){
+            System.out.println(i);
+            i = i + 2;
+        }
+    }
+    
+}
