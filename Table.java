1655045f075948bd51f1c409f3828c0d5d5ca198
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-06-27 17:32:27 +0530

    Create Table.java

---
+++ Table.java
@@ -0,0 +1,15 @@
+// TABLE OF n
+
+import java.util.*;
+
+
+public class Table {
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in); // CREATING SCANNER CLASS FORINPUT FROM USER
+        int n = sc.nextInt();
+        for(int i=1;i<=10;i++){ // LOOP
+        System.out.println(n*i);// OUTPUT STATEMENT
+        }
+    }
+
+}
