aa80c4e10de5b5df034e316777159151e62ee13c
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-06-27 17:24:09 +0530

    Create Dowhile.java

---
+++ Dowhile.java
@@ -0,0 +1,18 @@
+// do while
+
+
+
+
+public class Dowhileloop {
+    public static void main (String args[]){
+        int i = 0;
+        System.out.println("FIRST 10 EVEN NUMBERS \n");
+        do{
+            System.out.println(i);
+            i = i+2;
+            
+        }
+        while(i<=10);
+    }
+    
+}
