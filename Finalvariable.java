a32535b725c5daf88c06acf8ececd02c22c1b6ce
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-07-05 00:13:49 +0530

    Create Finalvariable.java

---
+++ Finalvariable.java
@@ -0,0 +1,13 @@
+public class Finalvariable {
+    final int carspeed = 60;
+    void run(){
+      //  carspeed = 90;
+      System.out.println("working");
+    }
+    public static void main(String args[]){
+        Finalvaruable c = new Finalvaruable();
+            c.run();
+
+    }
+}
+
