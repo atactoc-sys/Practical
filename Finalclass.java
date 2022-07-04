10be457891fa172ed91e4205da5d392368a26b3e
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-07-05 00:11:58 +0530

    Create Finalclass.java

---
+++ Finalclass.java
@@ -0,0 +1,19 @@
+final class Finalclass {
+    void display(){
+        System.out.println("working");
+    }
+  //  class Main extends Finalclass{
+  //      void display(){
+  //          System.out.println("it's working");
+  //      }
+  //  }
+  //public static void main(String args[]){
+  //      Main a = new Main();A
+  //      a.display();
+  //  }
+    public static void main(String args[]){
+        Finalclass a = new Finalclass();
+        a.display();
+    }
+
+}A
