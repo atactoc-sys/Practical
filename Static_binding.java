61bc8f214029a9eb8f595c17d749ded118365652
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-07-12 21:40:27 +0530

    Create Static_binding.java

---
+++ Static_binding.java
@@ -0,0 +1,9 @@
+public class Static_binding {
+    public void run(){
+        System.out.println("method is private");}
+        public static void main(String srgs[]) {
+            Static_binding obj = new Static_binding()
+            obj.run();
+        }
+    }
+}
