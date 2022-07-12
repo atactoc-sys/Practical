0d2b012fdf6dadcbe140e2ec63f6bed7b9f0160a
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-07-12 20:25:47 +0530

    Create Shlm11.java

---
+++ Shlm11.java
@@ -0,0 +1,12 @@
+public class Shlm11 {
+    private void run(){
+        System.out.println("60");
+       
+    }
+    public static void main(String args[]) {
+        Shlm11 obj = new Shlm11();
+        obj.run();
+        
+    }
+    
+}
