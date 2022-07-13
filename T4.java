723ed518f5720495a8d44477b0d3c8d030114579
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-07-13 23:24:56 +0530

    Create T4.java

---
+++ T4.java
@@ -0,0 +1,26 @@
+package com.emploiee;
+
+public class T4 {
+    public static void  main(String args[]){
+        Result4 obj = new Result4();
+        obj.setName("daku");
+        obj.setId("jkyuop");
+        obj.setPass("abcd");
+        obj.setA(65);
+        obj.setB(69);
+        obj.setC(89);
+        obj.setD(75);
+        obj.setE(82);
+        System.out.println(obj.getName());
+        System.out.println(obj.getId());
+        System.out.println(obj.getPass());
+        System.out.println(obj.getA());
+        System.out.println(obj.getB());
+        System.out.println(obj.getC());
+        System.out.println(obj.getD());
+        System.out.println(obj.getE());
+        obj.total();
+        obj.avv();
+
+    }
+}
