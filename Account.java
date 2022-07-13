51a8b4252c61b09bba80e4ecf0168d7b40fbbbf9
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-07-13 23:17:53 +0530

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,33 @@
+public class Account {
+    private String name;
+    private String email;
+    private long accountNumber;
+    private long ballance;
+    public String getName(){
+        return name;
+    
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public long getAccountNumber(){
+        return accountNumber;
+    }
+    public void setAccountNumber(long accountNumber){
+        this.accountNumber=accountNumber;
+    }
+    public long getBallance(){
+        return ballance;
+    }
+    public void setBallance(long ballance){
+        this.ballance=ballance;
+    }
+
+    
+}
