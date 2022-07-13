34d87864dab7125f18b0e5e6e611acf48de99e6a
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-07-13 23:23:53 +0530

    Create Result4.java

---
+++ Result4.java
@@ -0,0 +1,82 @@
+package com.emploiee;
+
+public class Result4 {
+    private String name;
+    private String id;
+    private String pass;
+    private int a;
+    private int b;
+    private int c;
+    private int d;
+    private int e;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public int getD() {
+        return d;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    public int getE() {
+        return e;
+    }
+
+    public void setE(int e) {
+        this.e = e;
+    }
+    void total(){
+        System.out.println(a+b+c+d+e);
+    }
+    void avv(){
+        System.out.println((a+b+c+d+e)/5);
+    }
+}
