f0541f0e5fe15e153c1c8fd94323f6332d931557
Saikat Mandi <104897172+atactoc-sys@users.noreply.github.com>
2022-07-13 23:19:35 +0530

    Create Test5.java

---
+++ Test5.java
@@ -0,0 +1,8 @@
+public class Test5 {
+    public static void main(String args[]){
+        Student_Encapsulate obj6 = new Student_Encapsulate();
+        obj6.setName("daku");
+        System.out.println(obj6.getName());
+        
+    }
+}
